1029275f92ec745c8f0e50b65e01b84b078fb9cf
zhouhuan2003 <109257747+zhouhuan2003@users.noreply.github.com>
2023-09-19 15:49:34 +0800

    Update OssTest.java

--- yygh_parent/service/service_oss/src/test/java/com/oss/test/OssTest.java
+++ yygh_parent/service/service_oss/src/test/java/com/oss/test/OssTest.java
@@ -14,8 +14,8 @@ public class OssTest {
         // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
         String endpoint = "https://oss-cn-hangzhou.aliyuncs.com";
         // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        String accessKeyId = "LTAI5tQk94WTwi1zYpyuNEmG";
-        String accessKeySecret = "vCCwAaw1xtIhjEEZUD6oxGdzQFzL6O";
+        String accessKeyId = "";
+        String accessKeySecret = "";
         // 填写Bucket名称，例如examplebucket。
         String bucketName = "yygh-testoss-ldzy-zhh";
 
